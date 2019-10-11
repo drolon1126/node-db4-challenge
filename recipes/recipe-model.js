7e50d31e94d5f8c0fded14cd6f07eff6301ac599
danRolon <daniel.rolon@yahoo.com>
2019-10-10 21:04:27 -0500

    fixed naming errors

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -16,7 +16,7 @@ function getShoppingList(id){
   .join('recipe_ingredients as ri', 'ri.ingredient_id', 'i.id')
   .join('recipes as r', 'r.id', 'ri.recipe_id')
   .where('r.id',id)
-  .select('r.recipe_name','i.ingredient_name','ri.quantity');
+  .select('r.recipeName','i.ingredientName','ri.quantity','ri.measurement')
 }
 
 function getInstructions(id){
@@ -24,7 +24,7 @@ function getInstructions(id){
     .join('recipes as r', 'r.id','s.recipe_id')
     .where('r.id',id)
     .orderBy('s.step_number')
-    .select('s.id','r.recipe_name','s.step_number','s.instructions');
+    .select('s.id','r.recipeName','s.stepNum','s.instructions');
 }
 
 function getIngredientRecipes(id){
@@ -32,5 +32,5 @@ function getIngredientRecipes(id){
   .join('recipe_ingredients as ri', 'ri.ingredient_id', 'i.id')
   .join('recipes as r', 'r.id', 'ri.recipe_id')
   .where('i.id',id)
-  .select('i.ingredient_name','r.recipe_name');
+  .select('i.ingredientName','r.recipeName');
 }
